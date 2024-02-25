087281fd78f4ff46884f431e85c1d94c699d6a30
Manny <mhfernan35277@gmail.com>
2024-02-25 15:35:43 -0700

    adds file

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
-
+useEffect(() => {
+  if(timeRemaining === 0) {
+     setTimeRemaining(10)
+     onAnswered(false)
+  }
+  
+  const timer = setTimeout(() => {
+    console.log("use effect called")
+    setTimeRemaining(timeRemaining - 1)}, 1000)
+  
+    return () => clearTimeout(timer)
+    
+  }, [timeRemaining, onAnswered])
+console.log("component rendering")
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
